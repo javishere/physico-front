2d5eb4a43eedd2136d468566e7575beedbe744e7
Javier Hortal Amador <jhortal@tychegroup.es>
2020-09-16 01:49:17 +0200

    Updates

---
+++ precache-manifest.e0e0e8c08539b444ba6807d985e043b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b31611971110c97ca7e4c2593a0da2d2",
+    "url": "/physico-front/index.html"
+  },
+  {
+    "revision": "e80cbd4f861c2322b5c3",
+    "url": "/physico-front/static/css/main.3ffd6df8.chunk.css"
+  },
+  {
+    "revision": "21f481d376bc58233862",
+    "url": "/physico-front/static/js/2.66ad6f1e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/physico-front/static/js/2.66ad6f1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e80cbd4f861c2322b5c3",
+    "url": "/physico-front/static/js/main.635d5ed5.chunk.js"
+  },
+  {
+    "revision": "ec3b327cd670abd3dec2",
+    "url": "/physico-front/static/js/runtime-main.309e70d7.js"
+  }
+]);
